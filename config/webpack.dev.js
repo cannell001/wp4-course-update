ad444df76b765cb4440ca98f90006a3a3bb1379e
Jeremy Cannell <jcannell007@hotmail.com>
2018-07-21 17:00:03 -0400

    Added image loaders Better error handling -browse

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -38,10 +38,26 @@ module.exports = {
                         }
                     },
                     {
-                        loader: "html-loader"
+                        loader: "html-loader",
+                        options: {
+                            attrs: ["img:src"]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(jpg|gif|png)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options:{
+                            name: "images/[name]-[hash:8].[ext]"
+
+                        }
                     }
                 ]
             }
+
         ]
         }
     }
