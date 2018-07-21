43aaea19a77d6378e191bc88200bc997b763bcc2
Jeremy Cannell <jcannell007@hotmail.com>
2018-07-21 16:32:26 -0400

    Added loaders for HTML files

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,26 +1,47 @@
 const path = require("path")
 
 module.exports = {
-  entry: {
-    main: "./src/index.js"
-  },
-  mode: "development",
-  output: {
-    filename: "[name]-bundle.js",
-    path: path.resolve(__dirname, "../dist/"),
-    publicPath: "/"
-  },
-  devServer: {
-    inline: true,
-    contentBase: "dist",
-    port: 1234
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
-      }
-    ]
-  }
-}
+    entry: {
+        main: "./src/index.js"
+    },
+    mode: "development",
+    output: {
+        filename: "[name]-bundle.js",
+        path: path.resolve(__dirname, "../dist/"),
+        publicPath: "/"
+    },
+    devServer: {
+        inline: true,
+        overlay: true,
+        contentBase: "dist",
+        port: 1234
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]"
+                        }
+                    },
+                    {
+                        loader: "extract-loader",
+                        options: {
+                            publicPath: "../"
+                        }
+                    },
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            }
+        ]
+        }
+    }
